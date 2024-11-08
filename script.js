bef5bfafe7f0b00734906db7b8a542b68066b9b1
Monaliza Cristina <monaliza.mca@gmail.com>
2024-11-08 10:17:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
 const multiplicationTable = document.querySelector("#multiplication-operations")
 
-// functions
-const creatTable = (number, multiplicatorNumber) => {
-    multiplicationTable.innerHTML = "";
-};
+// Function
 
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+
+        const result = number * i;
+
+        const template = `<div class="row">
+                <div class="operation">${number} x ${i} = </div>
+                <div class="result">${result}</div>
+            </div>`;
+
+        const parser = new DOMParser()
+
+        const htmlTemplate = parser.parseFromString(template, "text/html")
+
+        const row = htmlTemplate.querySelector(".row")
+
+        multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerText = number;
+};
 
 // events
 multiplicationForm.addEventListener("submit", (e) => {
@@ -21,5 +43,5 @@ multiplicationForm.addEventListener("submit", (e) => {
    
     if (!multiplicationNumber || !multiplicatorNumber) return;
 
-    console.log(multiplicationNumber, multiplicatorNumber);
+    createTable(multiplicationNumber, multiplicatorNumber);
 });
